23cf6fa9b6d398da504cccab489c617245cb2eaa
msurovceva <surovcevamarija@icloud.com>
2022-11-21 10:34:42 +0100

    erste commit

---
+++ src/test/java/de/neusta/backendchallenge/BackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neusta.backendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
